3562eb67d2017af232fa79ece22e2364421c9174
fabio.secci <fabiosecci@gmail.com>
2026-04-17 16:19:03 +0200

    feat: add 'Build shared truths' to design methodology

--- src/components/presentation/PresentationPage.jsx
+++ src/components/presentation/PresentationPage.jsx
@@ -307,7 +307,7 @@ const PresentationPage = () => {
         <div>Ask <Highlighter delay={1}>why.</Highlighter></div>
         <div>Validate at <Highlighter delay={1.3}>pace.</Highlighter></div>
         <div>Hypothesize, experiment, <Highlighter delay={1.6}>iterate.</Highlighter></div>
-        <div>Prove or disprove, <Highlighter delay={1.9}>document.</Highlighter></div>
+        <div>Prove or disprove, document. <Highlighter delay={1.9}>Build shared truths.</Highlighter></div>
       </div>,
       background: "white",
       color: "black"
