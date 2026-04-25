b5e78fc8a8c21cf4393e5e308d1b30b38ec3d778
fabio.secci <fabiosecci@gmail.com>
2026-04-25 12:24:54 +0200

    Change Experience slide right-side animation to slide-in

--- src/components/presentation/PresentationPage.jsx
+++ src/components/presentation/PresentationPage.jsx
@@ -818,10 +818,10 @@ const PresentationPage = () => {
                       </motion.div>
                       
                       <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 1.2 }}
+                        initial={{ x: '100%' }}
+                        animate={{ x: 0 }}
+                        exit={{ x: '100%' }}
+                        transition={{ duration: 1.2, ease: APPLE_EASE }}
                         style={{
                           position: 'absolute',
                           top: 0,
